26146839dd1ea9f12c95953d70fda08818427d92
luvmemore <sophia7625@naver.com>
2024-02-06 17:47:41 +0900

    수정

--- working/working/src/main/java/com/work/working/exception/BoardErrorCode.java
+++ working/working/src/main/java/com/work/working/exception/BoardErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum BoardErrorCode implements CommonErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND, "E-404", "Partner Id Not Found."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "E-404", "Board Id Not Found."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E-500", "Internal Server Error.");
 
     private final HttpStatus status;
